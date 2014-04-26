0ce8e8cf2e3696f82af2fdd1e00a6b4e4fdb3c1e
Jason Frame <jason@onehackoranother.com>
2014-04-26 13:59:37 +0100

    analytics, remove unused font

--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,7 @@
 <html>
 <head>
   <title>Dead Coders Society</title>
-  <link href='http://fonts.googleapis.com/css?family=Princess+Sofia|Over+the+Rainbow' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Princess+Sofia' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" type="text/css" href="assets/main.css">
 </head>
 <body>
@@ -53,6 +53,16 @@
     </div>
 
   </div>
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-41562396-3', 'deadcoders.org');
+    ga('send', 'pageview');
+  </script>
 
 </body>
 </html>
