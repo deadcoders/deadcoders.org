2afcc62b4c6db4de868dbf041894f573672d66e0
Jason Frame <jason@onehackoranother.com>
2014-04-26 13:54:16 +0100

    design!

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
   <title>Dead Coders Society</title>
+  <link href='http://fonts.googleapis.com/css?family=Princess+Sofia|Over+the+Rainbow' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" type="text/css" href="assets/main.css">
 </head>
 <body>
@@ -9,6 +10,46 @@
 
     <div id='header'>
       <h1>Dead Coders Society</h1>
+    </div>
+
+    <section>
+      <h2>What?</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </section>
+
+    <section>
+      <h2>Where?</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </section>
+
+    <section>
+      <h2>When?</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </section>
+
+    <div class='logo'>
+      <img src='assets/logo.png'>
     </div>
 
   </div>
