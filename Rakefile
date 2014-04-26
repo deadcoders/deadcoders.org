502795f5c0dd7b998d84df05e11bdc9db484a598
Jason Frame <jason@onehackoranother.com>
2014-04-26 12:28:24 +0100

    stuff

--- Rakefile
+++ Rakefile
@@ -1,3 +1,7 @@
 task :deploy do
   sh "ssh -A deadcoders.org 'cd sites/deadcoders.org && git pull origin master'"
 end
+
+task :serve do
+  sh "php -S localhost:8000 -t public_html"
+end
--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,17 @@
-<?= "yarr" ?>
+<html>
+<head>
+  <title>Dead Coders Society</title>
+  <link rel="stylesheet" type="text/css" href="assets/main.css">
+</head>
+<body>
+
+  <div id='container'>
+
+    <div id='header'>
+      <h1>Dead Coders Society</h1>
+    </div>
+
+  </div>
+
+</body>
+</html>
